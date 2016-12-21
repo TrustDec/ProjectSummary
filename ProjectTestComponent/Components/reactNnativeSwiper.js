a89c3f2708e694612f04b94d5c1a237683d2bed5
TRUST\TrustTheBoy <603049583@qq.com>
2016-12-21 23:53:35 +0800

    no message

--- ProjectTestComponent/Components/reactNnativeSwiper.js
+++ ProjectTestComponent/Components/reactNnativeSwiper.js
@@ -5,8 +5,35 @@ import {
   Text,
   View
 } from 'react-native';
-
+/* https://github.com/leecade/react-native-swiper#custom-basic-style--content */
 import Swiper from 'react-native-swiper';
+
+let _swiper = {
+  index:0,
+  loop:false,
+  horizontal:true,
+  showsButtons:false,
+  autoplay:false,
+  loadMinimal:false
+};
+
+export default class SwiperDome extends Component {
+  render() {
+    return (
+     <Swiper style={styles.wrapper} {..._swiper}>
+        <View style={styles.slide1}>
+          <Text style={styles.text}>呵呵</Text>
+        </View>
+        <View style={styles.slide2}>
+          <Text style={styles.text}>哈哈</Text>
+        </View>
+        <View style={styles.slide3}>
+          <Text style={styles.text}>嘻嘻</Text>
+        </View>
+      </Swiper>
+    );
+  }
+}
 
 var styles = StyleSheet.create({
   wrapper: {
@@ -35,21 +62,3 @@ var styles = StyleSheet.create({
     fontWeight: 'bold',
   }
 })
-
-export default class SwiperDome extends Component {
-  render() {
-    return (
-     <Swiper style={styles.wrapper} showsButtons={true}>
-        <View style={styles.slide1}>
-          <Text style={styles.text}>呵呵</Text>
-        </View>
-        <View style={styles.slide2}>
-          <Text style={styles.text}>哈哈</Text>
-        </View>
-        <View style={styles.slide3}>
-          <Text style={styles.text}>嘻嘻</Text>
-        </View>
-      </Swiper>
-    );
-  }
-}
